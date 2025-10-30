a16593a6349a089619a0ba6508dd3e2020b82ddb
kamdem Brice <kamdembrice770@gmail.com>
2025-10-31 00:59:02 +0100

    création de mon premier endpoint

--- project/project/settings.py
+++ project/project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-fc#b%@wpq!_!ox0xa=a!hrz*j3cd=p+sz=g@8#wl45jionu7&c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
--- project/project/urls.py
+++ project/project/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from shop.views import CategoryAPIView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls'))
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/category/', CategoryAPIView.as_view())
 ]
